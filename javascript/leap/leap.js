f728a68dd64a8b65e3cae63da3518fb5d9b6d928
Mark Wong <marky.wong@gmail.com>
2018-11-13 20:25:23 +0000

    Some refactoring

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,20 +1,14 @@
 //
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const isLeap = (year) => {
   const isDivisibleBy4 = (year % 4 === 0);
 
-  if (isDivisibleBy4 === true) {
+  if (isDivisibleBy4) {
     const isDivisibleBy100 = (year % 100 === 0);
     const isDivisibleBy400 = (year % 400 === 0);
 
-    if (isDivisibleBy100 === true && isDivisibleBy400 === false) {
-      return false;
+    if (!isDivisibleBy100 || isDivisibleBy400) {
+      return true;
     }
-
-    return true;
   }
   return false;
 };
