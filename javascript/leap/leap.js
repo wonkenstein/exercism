f99055ca7658e209ee7e9451cc9ee3016b94faf2
Mark Wong <marky.wong@gmail.com>
2018-11-13 20:34:15 +0000

    Refactoring

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,14 +1,7 @@
 //
 export const isLeap = (year) => {
-  const isDivisibleBy4 = (year % 4 === 0);
-
-  if (isDivisibleBy4) {
-    const isDivisibleBy100 = (year % 100 === 0);
-    const isDivisibleBy400 = (year % 400 === 0);
-
-    if (!isDivisibleBy100 || isDivisibleBy400) {
-      return true;
-    }
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    return true;
   }
   return false;
 };
