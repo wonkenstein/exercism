bb0f2e50289aa2094084e32f2223ab06eace8a67
Mark Wong <marky.wong@gmail.com>
2018-11-13 20:51:56 +0000

    Refactoring leap.js

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,7 +1,4 @@
 //
 export const isLeap = (year) => {
-  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
-    return true;
-  }
-  return false;
+  return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
 };
