d34b87a7fef7462f0813e9e4b1f86c503e89db52
Mark Wong <marky.wong@gmail.com>
2018-11-16 21:07:31 +0000

    Include 2 solutions

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -16,3 +16,24 @@ export const steps = (input, step = 0) => {
 
   return steps(inputNum, (step + 1));
 };
+
+export const steps2 = (input) => {
+  let inputNum = parseInt(input, 10);
+
+  if (inputNum < 1 || Number.isNaN(inputNum)) {
+    throw new Error('Only positive numbers are allowed');
+  }
+
+  let step = 0;
+  while (inputNum !== 1) {
+    if (inputNum % 2 === 0) {
+      inputNum /= 2;
+    } else {
+      inputNum = (3 * inputNum) + 1;
+    }
+    inputNum = parseInt(inputNum, 10);
+    step += 1;
+  }
+
+  return step;
+};
