56598afec1cfea8328cde758a99625cb5ab4a745
Mark Wong <marky.wong@gmail.com>
2018-11-16 21:07:13 +0000

    Add leap.js modification

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,4 +1,23 @@
 //
 export const isLeap = (year) => {
-  return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
+  const divisibleBy4 = year % 4 === 0;
+  if (!divisibleBy4) {
+    return false;
+  }
+
+  const divisibleBy100 = year % 100 === 0;
+  if (!divisibleBy100) {
+    return true;
+  }
+
+  const divisibleBy400 = year % 400 === 0;
+  if (!divisibleBy400) {
+    return false;
+  }
+
+  return true;
 };
+
+// export const isLeap = (year) => {
+//   return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
+// };
