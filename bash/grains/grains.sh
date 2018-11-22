bbe7ec441b0ec483ff14b7e52339e3a75d33eb00
Mark Wong <marky.wong@gmail.com>
2018-11-22 23:05:31 +0000

    Refactoring and updating after comments from mentor

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -5,18 +5,16 @@ main() {
   # you can use input="$@" to get a string with ALL arguments
   limit=$1
   isNumber='^[0-9]+$'
-  let ratio=2
+  ratio=2
 
   if [[ $limit = 'total' ]]; then
     # https://www.mathsisfun.com/algebra/sequences-sums-geometric.html
     # shortcut to total the amount ((2^64)-1)
-    let limit=64
-    total=$(bc <<< "$ratio^$limit-1")
-    echo $total
+    limit=64
+    bc <<< "($ratio^$limit) - 1"
     exit 0
-  elif [[ "$limit" =~ $isNumber ]] && [ $limit -gt 0 ] && [ $limit -le 64 ]; then
-    total=$(bc <<< "$ratio^($limit-1)")
-    echo $total
+  elif [[ $limit =~ $isNumber ]] && [ $limit -gt 0 ] && [ $limit -le 64 ]; then
+    bc <<< "$ratio^($limit - 1)"
     exit 0
   fi
 
