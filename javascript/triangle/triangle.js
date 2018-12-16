6259724fa8e25b6b9bb5169d1f51279ff2561598
Mark Wong <marky.wong@gmail.com>
2018-12-16 00:34:56 +0000

    Refactoring triangles

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -9,52 +9,58 @@ export class Triangle {
   }
 
   kind() {
-    if (this.isTriangle() === true) {
-      const diffLengths = this.lengths.reduce((acc, length) => {
-        acc[length] = length;
-        return acc;
-      }, {});
+    const hasIllegalLength = this.hasIllegalLength();
 
-      const numberOfDiffLengths = Object.keys(diffLengths).length;
-
-      let typeOfTriangle = '';
-      if (numberOfDiffLengths === 1) {
-        typeOfTriangle = 'equilateral';
-      } else if (numberOfDiffLengths === 2) {
-        typeOfTriangle = 'isosceles';
-      } else if (numberOfDiffLengths === 3) {
-        typeOfTriangle = 'scalene';
-      }
-
-      return typeOfTriangle;
+    if (hasIllegalLength !== false) {
+      throw new Error('triangle has illegal length');
     }
+
+    const isTriangle = this.isTriangle();
+    if (isTriangle !== true) {
+      throw new Error('triangle is fails triangle inequality');
+    }
+
+    const diffLengths = this.lengths.reduce((acc, length) => {
+      acc[length] = length;
+      return acc;
+    }, {});
+
+    const numberOfDiffLengths = Object.keys(diffLengths).length;
+
+    let typeOfTriangle = '';
+    if (numberOfDiffLengths === 1) {
+      typeOfTriangle = 'equilateral';
+    } else if (numberOfDiffLengths === 2) {
+      typeOfTriangle = 'isosceles';
+    } else if (numberOfDiffLengths === 3) {
+      typeOfTriangle = 'scalene';
+    }
+
+    return typeOfTriangle;
   }
 
-  hasNegativeLength() {
+  hasIllegalLength() {
     const hasIllegalLength = this.lengths.filter(length => (length <= 0));
 
     if (hasIllegalLength.length) {
-      throw new Error('triangle with negative sides are illegal');
+      return true;
     }
+
+    return false;
   }
 
   isTriangle() {
-    const hasIllegalLength = this.lengths.filter(length => (length <= 0));
-
-    if (hasIllegalLength.length) {
-      throw new Error('triangle with negative sides are illegal');
-    }
-
+    let isTriangle = true;
     if ((this.lengths[0] + this.lengths[1]) < this.lengths[2]) {
-      throw new Error('Illegal triangle');
+      isTriangle = false;
     }
     if ((this.lengths[1] + this.lengths[2]) < this.lengths[0]) {
-      throw new Error('Illegal triangle');
+      isTriangle = false;
     }
     if ((this.lengths[2] + this.lengths[3]) < this.lengths[1]) {
-      throw new Error('Illegal triangle');
+      isTriangle = false;
     }
 
-    return true;
+    return isTriangle;
   }
 }
