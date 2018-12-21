a729e6061add288fbe47ad6507fada4e2d93b6e5
Mark Wong <marky.wong@gmail.com>
2018-12-21 23:54:07 +0000

    Add bob exercise

---
+++ javascript/bob/bob.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+//
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const hey = (message) => {
+  //
+  // YOUR CODE GOES HERE
+  //
+};
