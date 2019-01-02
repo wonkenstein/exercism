fc4cd292696abf3e7e7c8f869c7307886b48a90c
Mark Wong <marky.wong@gmail.com>
2019-01-02 22:57:14 +0000

    Add bob solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -4,8 +4,41 @@
 // convenience to get you started writing code faster.
 //
 
+const isSilence = (message) => {
+  const isWhiteSpace = /^\s+$/;
+  return (message === '' || isWhiteSpace.test(message));
+};
+
+const isShouting = (message) => {
+  const toUpperCase = message.toUpperCase();
+  const hasAlpha = /[A-Za-z]/;
+  return (hasAlpha.test(message) && message === toUpperCase);
+};
+
+const isAskingQuestion = (message) => {
+  const question = /\?$/;
+  return question.test(message);
+};
+
 export const hey = (message) => {
-  //
-  // YOUR CODE GOES HERE
-  //
+  message = message.trim();
+
+  let response = 'Whatever.';
+
+  if (isSilence(message)) {
+    response = 'Fine. Be that way!';
+  } else {
+    const shouting = isShouting(message);
+    const askingQuestion = isAskingQuestion(message);
+
+    if (shouting && askingQuestion) {
+      response = "Calm down, I know what I'm doing!";
+    } else if (shouting) {
+      response = 'Whoa, chill out!';
+    } else if (askingQuestion) {
+      response = 'Sure.';
+    }
+  }
+
+  return response;
 };
