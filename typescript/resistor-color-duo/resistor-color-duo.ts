f9c6ab9eec1d56a9e89730f86962492a2f49776b
Mark Wong <marky.wong@gmail.com>
2022-09-01 14:04:18 +0100

    Add  typescript/resistor-color-duo solution

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,31 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+
+type ResistorColorValues = {
+  [key: string]: number
+}
+
+const RESISTOR_COLOR_VALUES: ResistorColorValues = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9,
+}
+export function decodedValue(colors: string[]): number {
+  // just take the first two colors
+  const resistorColors = colors.slice(0, 2)
+
+  const result: number[] = resistorColors.reduce((acc: number[], resistorColor: string): number[] => {
+    const resistorValue = RESISTOR_COLOR_VALUES[resistorColor.toLowerCase()]
+    acc.push(resistorValue)
+    return acc
+  }, [])
+
+  // convert to number https://stackoverflow.com/a/14668510/1612219
+  const decoded = +result.join('')
+  return decoded
 }
