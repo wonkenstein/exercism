e31ee52fa4fb92c160c438f7fcc0403d4d2ea938
Mark Wong <marky.wong@gmail.com>
2022-09-02 17:08:53 +0100

    Randomly generate name - if generates an existing one, try and sequentially get the next one

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -2,26 +2,86 @@
 
 let ROBOT_NAMES: any = {}
 let ROBOT_NAME_PREFIXES: any = {}
+const ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const NUMERIC = '0123456789'
+const NUM_ALPHA = 2
+const NUM_NUMERIC = 3
 
 const generateRobotName = (): string => {
-  const alpha: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  const numeric = '0123456789'
-  const NUM_ALPHA = 2
-  const NUM_NUMERIC = 3
+  // const chars: string[] = []
 
-  const chars: string[] = []
+  // for (let i = 0; i < NUM_ALPHA; i++) {
+  //   chars.push(randomise(ALPHA))
+  // }
+  // for (let i = 0; i < NUM_NUMERIC; i++) {
+  //   chars.push(randomise(NUMERIC))
+  // }
 
-  for (let i = 0; i < NUM_ALPHA; i++) {
-    chars.push(randomise(alpha))
-  }
-  for (let i = 0; i < NUM_NUMERIC; i++) {
-    chars.push(randomise(numeric))
-  }
+  // const nameCode = chars.join('')
+  let name = ''
+  do {
+    name = randomRobotName(name)
+  } while (robotNameIsUsed(name))
 
-  const nameCode = chars.join('')
-  return nameCode
+  burnRobotName(name)
+  return name
 }
 
+const randomRobotName = (input: string = ''): string => {
+  const chars: string[] = []
+  const prefix = getRobotNamePrefix(input)
+  if (prefix) {
+    const numbers = +getRobotNameNumbers(input)
+    // check the count of the codes for a prefix
+    if (ROBOT_NAME_PREFIXES[prefix] >= 999) {
+      // increment the second alpha
+      let alpha1Index = ALPHA.indexOf(prefix[0])
+      let alpha2Index = ALPHA.indexOf(prefix[1]) + 1
+      if (alpha2Index > 25) {
+        if (alpha1Index > 25) {
+          alpha1Index = 0
+        }
+      }
+
+      // const incrementedPrefix =
+      chars.push(ALPHA[alpha1Index])
+      chars.push(ALPHA[alpha2Index])
+      chars.push(String(numbers))
+
+    } else {
+      chars.push(prefix)
+      // increment the numbers
+      if (numbers >= 999) {
+        chars.push('0'.padStart(3, '0'))
+      } else {
+        chars.push(String(numbers + 1).padStart(3, '0'))
+      }
+    }
+
+  } else {
+    for (let i = 0; i < NUM_ALPHA; i++) {
+      chars.push(randomise(ALPHA))
+    }
+    for (let i = 0; i < NUM_NUMERIC; i++) {
+      chars.push(randomise(NUMERIC))
+    }
+  }
+
+  return chars.join('')
+}
+
+const getRobotNamePrefix = (robotName: string): string => {
+  if (robotName.length < 2) {
+    return ''
+  }
+  return robotName.slice(0, 2)
+}
+const getRobotNameNumbers = (robotName: string): string => {
+  if (robotName.length < 5) {
+    return ''
+  }
+  return robotName.slice(2, 5)
+}
 const randomise = (dataSet: string): string => {
   const max = dataSet.length
   const randomIndex = Math.floor(Math.random() * max)
@@ -30,6 +90,12 @@ const randomise = (dataSet: string): string => {
 
 const burnRobotName = (robotName: string): void => {
   ROBOT_NAMES[robotName] = 1
+  const prefix = getRobotNamePrefix(robotName)
+  if (ROBOT_NAME_PREFIXES[prefix] === undefined) {
+    ROBOT_NAME_PREFIXES[prefix] = 1
+  } else {
+    ROBOT_NAME_PREFIXES[prefix]++
+  }
 }
 
 const robotNameIsUsed = (robotName: string): boolean => {
@@ -41,6 +107,7 @@ const clearRobotNames = (): void => {
   ROBOT_NAME_PREFIXES = {}
 }
 
+/////////////////////////////////
 export class Robot {
   private currentName: string
 
@@ -54,17 +121,17 @@ export class Robot {
   }
 
   public resetName(): void {
-    let newName: string
-
-    do {
-      newName = generateRobotName()
-    } while (robotNameIsUsed(newName))
-
-    this.currentName = newName
-    burnRobotName(newName)
+    this.currentName = generateRobotName()
   }
 
   public static releaseNames(): void {
     clearRobotNames()
+  }
+
+  public debug(): void {
+    const names = Object.keys(ROBOT_NAMES)
+    names.sort()
+    console.log(names)
+    // console.log(ROBOT_NAME_PREFIXES)
   }
 }
