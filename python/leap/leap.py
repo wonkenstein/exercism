bb5773cac4614c879103b5b2f5b7c10ec73ef253
Mark Wong <marky.wong@gmail.com>
2022-09-11 22:47:32 +0100

    Add python:leap

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,12 @@
 def is_leap_year(year):
-    pass
+    # pass
+    divisible_by_4 = (year % 4 == 0)
+
+    if (divisible_by_4):
+        divisible_by_100 = (year % 100 == 0)
+        divisible_by_400 = (year % 400 == 0)
+        # except every year that is evenly divisible by 100
+        if (divisible_by_100 and not divisible_by_400):
+            return False
+        return True
+    return False
