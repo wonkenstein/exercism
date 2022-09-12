7cacfc8a2206093b1344f24a41d05efaf12ace82
Mark Wong <marky.wong@gmail.com>
2022-09-12 01:42:14 +0100

    remove TODO

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -1,16 +1,17 @@
 """Functions used in preparing Guido's gorgeous lasagna.
 
-Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
+Learn about Guido, the creator of the Python language:
+https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
+# define the 'EXPECTED_BAKE_TIME' constant
 EXPECTED_BAKE_TIME = 40
 
-# TODO: consider defining the 'PREPARATION_TIME' constant
+# consider defining the 'PREPARATION_TIME' constant
 #       equal to the time it takes to prepare a single layer
 PREPARATION_TIME = 2
 
-# TODO: define the 'bake_time_remaining()' function
+# define the 'bake_time_remaining()' function
 def bake_time_remaining(time_elapsed=0):
     """Calculate the bake time remaining.
 
@@ -23,10 +24,8 @@ def bake_time_remaining(time_elapsed=0):
     """
 
     return EXPECTED_BAKE_TIME - time_elapsed
-    # pass
 
-
-# TODO: define the 'preparation_time_in_minutes()' function
+# define the 'preparation_time_in_minutes()' function
 #       and consider using 'PREPARATION_TIME' here
 def preparation_time_in_minutes(num_layers=1):
     """Calculate the preparation time.
@@ -35,9 +34,8 @@ def preparation_time_in_minutes(num_layers=1):
     :return: int - time taken to prepare the layers (in minutes) derived from 'PREPARATION_TIME'.
     """
     return PREPARATION_TIME * num_layers
-    # pass
 
-# TODO: define the 'elapsed_time_in_minutes()' function
+# define the 'elapsed_time_in_minutes()' function
 def elapsed_time_in_minutes(layers=0, bake_time=0):
     """Calculate the total elapsed time.
 
@@ -46,4 +44,3 @@ def elapsed_time_in_minutes(layers=0, bake_time=0):
     :return: int - calculate elapsed time taken to prepare and bake cake.
     """
     return preparation_time_in_minutes(layers) + bake_time
-    # pass
