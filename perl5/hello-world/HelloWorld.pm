9a8cf09ad97c68f668a0babfbd7cc489b590088b
Mark Wong <marky.wong@gmail.com>
2022-09-27 20:10:06 +0100

    Add perl5:hello-world

---
+++ perl5/hello-world/HelloWorld.pm
@@ -0,0 +1,15 @@
+# Declare package 'HelloWorld'
+package HelloWorld;
+
+use strict;
+use warnings;
+use feature qw<say>;    # Use the `say` function to output debugging info in the online editor.
+
+use Exporter qw<import>;
+our @EXPORT_OK = qw<hello>;
+
+sub hello {
+    return 'Goodbye, Mars!';
+}
+
+1;
