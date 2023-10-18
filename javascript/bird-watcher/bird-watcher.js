32b4cf1b8e90ea5f967f58c81ccf2e37f6357070
Mark Wong <marky.wong@gmail.com>
2023-10-18 22:17:46 +0100

    Add bird-watcher

---
+++ javascript/bird-watcher/bird-watcher.js
@@ -0,0 +1,53 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+  let total = 0;
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    total += birdsPerDay[i];
+  }
+  return total;
+  // return birdsPerDay.reduce((acc, item) => (acc += item), 0);
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+  // throw new Error("Please implement the birdsInWeek function");
+  const DAYS_IN_WEEK = 7;
+  const start = week === 1 ? 0 : (week - 1) * DAYS_IN_WEEK;
+
+  const weekToCount = birdsPerDay.slice(start, start + DAYS_IN_WEEK);
+
+  return totalBirdCount(weekToCount);
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    if (i % 2 === 0) {
+      birdsPerDay[i] += 1;
+    }
+  }
+  return birdsPerDay;
+}
